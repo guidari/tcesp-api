a97dd1646cd6d3c3c897a3689b24cc62f58f95bb
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2021-10-22 21:46:31 -0300

    GET working after added CORS

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { municipioRoute } from "./municipio.routes.js";
 import { logRoute } from "./log.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-app.use("/logs", logRoute);
 
+app.use(cors({ origin: "http://localhost:3000" }));
+
+app.use("/logs", logRoute);
 app.use("/municipios", municipioRoute);
 
 const port = process.env.PORT || 3333;
